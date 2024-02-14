308997a3f2ea4eba0cf4ba3bbcd3c1e8539cb8ef
1manuelc <manuelwn21@gmail.com>
2024-02-14 15:40:21 -0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
-accordionList.forEach((item, itemIndex) => {
-	item.addEventListener('click', () => {
-		updateAccordion(item, itemIndex);
+(async function main() {
+	accordionList.forEach((item, itemIndex) => {
+		item.addEventListener('click', () => {
+			updateAccordion(item, itemIndex);
+		});
 	});
-});
 
-(async function updateRepos() {
-	const htmlList = document.querySelector('#repositories');
-	const reposList = await getRepos();
-	let reposString = reposList
-		.map((repo) => {
-			return `
-		<li>
-			<span class="repo__header">
-				<i class="fa-brands fa-github"></i>
-				<h4>${repo.name}</h4></span
-			>
-			<a class="repo__link" 
-				target="_blank"
-				href="${repo.link}">${repo.link}
-			</a>
-		</li>
-		`;
-		})
-		.join('\n');
-
-	htmlList.innerHTML = reposString;
+	const profile = await getProfile();
+	updatePageInfos(profile);
+	console.log(profile);
 })();
